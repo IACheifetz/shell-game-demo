600c38d7779da22e16a3298a87e75cacfb438b95
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-08 11:04:56 -0800

    added click functionality to all buttons and added function to track number of wins losses and total attempts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
-const buttonOne = document.getElementById('cupOneBtn');
-const buttonTwo = document.getElementById('cupTwoBtn');
-const buttonThree = document.getElementById('cupThreeBtn');
+const btnOne = document.getElementById('cupOneBtn');
+const btnTwo = document.getElementById('cupTwoBtn');
+const btnThree = document.getElementById('cupThreeBtn');
 
 const cupOne = document.getElementById('cup-one');
 const cupTwo = document.getElementById('cup-two');
@@ -15,13 +15,71 @@ let correct = 0;
 let total = 0;
 
 function resetCups() {
-  cupOne.src = './assets/cupgame1.png';
-  cupTwo.src = './assets/cupgame1.png';
-  cupThree.src = './assets/cupgame1.png';
+    cupOne.src = './assets/cupgame1.png';
+    cupTwo.src = './assets/cupgame1.png';
+    cupThree.src = './assets/cupgame1.png';
+}
+
+function statusDisplay() {
+    correctEl.textContent = correct;
+    totalEl.textContent = total;
+    incorrectEl.textContent = total - correct;
 }
 // set event listeners 
 
+btnOne.addEventListener('click', () => {
+    resetCups();
+    total++;
+    const rdmCupNmb = Math.floor(Math.random() * 3);
 
+    if (rdmCupNmb === 0) {
+        cupOne.src = '/assets/cupgame2.png';
+        correct++;
+    } else if (rdmCupNmb === 1) {
+        cupTwo.src = '/assets/cupgame2.png';
+    } else {
+        cupThree.src = '/assets/cupgame2.png';
+    }
+
+    statusDisplay();
+
+});
+
+btnTwo.addEventListener('click', () => {
+    resetCups();
+    total++;
+    const rdmCupNmb = Math.floor(Math.random() * 3);
+
+    if (rdmCupNmb === 0) {
+        cupOne.src = '/assets/cupgame2.png';
+    } else if (rdmCupNmb === 1) {
+        cupTwo.src = '/assets/cupgame2.png';
+        correct++;
+    } else {
+        cupThree.src = '/assets/cupgame2.png';
+    }
+    
+    statusDisplay();
+
+});
+
+btnThree.addEventListener('click', () => {
+    resetCups();
+    total++;
+    const rdmCupNmb = Math.floor(Math.random() * 3);
+
+    if (rdmCupNmb === 0) {
+        cupOne.src = '/assets/cupgame2.png';
+    } else if (rdmCupNmb === 1) {
+        cupTwo.src = '/assets/cupgame2.png';
+    } else {
+        cupThree.src = '/assets/cupgame2.png';
+        correct++;
+    }
+
+    statusDisplay();
+
+});
 
 
 
